89a4080b350185e0adc94f987aa5b903c71f1f9d
amiltonbandeira <amiltonbraulioadaobandeira@gmail.com>
2025-06-26 16:32:33 +0100

    [NEW] create __manifest__ file

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': "Awesome Owl",
+    'description': """
+        Tutorials for the beginning to domain the owl framework
+    """,
+    'summary': "Making a route to display an intro to odoo web framwework",
+    'category': "Owl/Awesome",
+    'author': "Amilton Bandeira",
+    'odoo-version': "18.0",
+    'depends': [
+        'web',
+    ],
+    'data': [
+        '',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'awesome_owl/static/src/awesome_owl.js',
+            'awesome_owl/static/src/awesome_owl.xml',
+        ],
+    },
+    'license': "AGPL-3",
+    'installble': True,
+    'application': True,
+    'auto_install': False,
+}
